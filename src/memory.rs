382edcdf29ffe089c21a2b449592237034873e67
achuvyas-kv <achu@keyvalue.systems>
2025-08-28 14:26:56 +0530

    Add ownership and reference concepts in memory.rs; introduce new SVG file for visual representation of Rust memory management.

--- src/memory.rs
+++ src/memory.rs
@@ -39,3 +39,16 @@ fn modify_numbers(nums: &mut Vec<i32>) {
 // println!("Immutable: {:?}", immutable_numbers); // Error!
 // println!("Mutable: {:?}", mutable_numbers);
 //
+//
+//
+
+// Ownership
+// Each value in Rust has an owner.
+// There can only be one owner at a time.
+// When the owner goes out of scope, the value will be dropped.
+//
+//
+
+// References
+// At any given time, you can have either one mutable reference or any number of immutable references.
+// References must always be valid.
